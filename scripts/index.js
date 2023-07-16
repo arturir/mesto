abb3a495ff946a45809d9f6c512ed047bc04dfbf
Artur Ibragimov <artifakt666@yandex.ru>
2023-07-16 18:06:15 +0300

    hotfixes: рефакторинг кода соглсан рекомендациям

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,17 @@
-const addButton = document.querySelector(".profile__add-button"),
+const addImageButton = document.querySelector(".profile__add-button"),
     editButton = document.querySelector(".profile__edit"),
     profileName = document.querySelector(".profile__name"),
     profileMetier = document.querySelector(".profile__metier"),
-    popupWindow = document.querySelector(".popup"),
-    popupImage = document.querySelector(".popup-image"),
+    popupProfileEditor = document.querySelector(".popup_profile-editor"),
+    popupNewCard = document.querySelector(".popup_new-card"),
+    popupImage = document.querySelector(".popup_image"),
     formName = document.querySelector("input[name='name']"),
     formMetier = document.querySelector("input[name='metier']"),
     formPlace = document.querySelector("input[name='place']"),
     formLink= document.querySelector("input[name='link']"),
     formProfile = document.querySelector(".form_profile"),
     formNewPlace = document.querySelector(".form_new-place"),
-    formCloser = document.querySelector(".popup__close"),
+    popupClosers = document.querySelectorAll(".close-icon"),
     imageCloser = document.querySelector(".popup-image__close"),
     body = document.querySelector(".body"),
     cards = document.querySelector(".cards"),
@@ -42,57 +43,65 @@ const addButton = document.querySelector(".profile__add-button"),
           link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
         }
       ];
-let activeForm;
-function openOrCloseForm (popupWindow, form) {
-    popupWindow.classList.toggle("popup_active");
-    form.classList.toggle("form_active");
-    body.classList.toggle("body_no-scroll");
-    if (form.classList.contains("form_profile") && popupWindow.classList.contains("popup_active")) {
-        formName.value = profileName.textContent;
-        formMetier.value = profileMetier.textContent;
-    }
-    activeForm = form;
+let imageInPopupImage = document.querySelector(".gallery__image"),
+    titleInPopupImage = document.querySelector(".gallery__title");
+
+function handleLikeClick (event) {
+  event.target.classList.toggle("card__like_active");
 }
-function saveProfile () {
-    event.preventDefault();
-    profileName.textContent = formName.value;
-    profileMetier.textContent = formMetier.value;
-    openOrCloseForm(popupWindow, formProfile);
+function handleDeleteClick (event) {
+  event.target.parentNode.remove();
 }
-function renderCard (card) {
+function handleImageClick (event) {
+  openImage(event.target);
+}
+function createCard (card) {
     newCard = templateCard.cloneNode(true);
     newCard.querySelector(".card__image").src = card.link;
     newCard.querySelector(".card__image").alt = card.name;
     newCard.querySelector(".card__title").textContent = card.name;
-    newCard.querySelector(".card__like").addEventListener("click", (element)=> {
-        element.target.classList.toggle("card__like_active");
-    });
-    newCard.querySelector(".card__delete").addEventListener("click", (element)=> {
-        element.target.parentNode.remove();
-    });
-    newCard.querySelector(".card__image").addEventListener("click", (element)=> {
-        openImage(element.target);
-    });
-    cards.prepend(newCard);
+    newCard.querySelector(".card__like").addEventListener("click", handleLikeClick);
+    newCard.querySelector(".card__delete").addEventListener("click", handleDeleteClick);
+    newCard.querySelector(".card__image").addEventListener("click", handleImageClick);
+    return newCard;
+}    
+function renderCard (card) {
+  cards.prepend(createCard(card));
 }
-function addNewCard () {
+function openPopup (popup) {
+  popup.classList.add("popup_active");
+  body.classList.add("body_no-scroll");
+  if (!popup.classList.contains("popup_image") && popup.querySelector("form").classList.contains("form_profile")) {
+    formName.value = profileName.textContent;
+    formMetier.value = profileMetier.textContent;
+  }
+}
+function closePopup (event) {
+  if (event.currentTarget.parentNode.parentNode.classList.contains("popup_active")) {event.currentTarget.parentNode.parentNode.classList.remove("popup_active")}
+  else {event.currentTarget.parentNode.parentNode.parentNode.classList.remove("popup_active")}
+  body.classList.remove("body_no-scroll");
+}
+function saveProfile (event) {
     event.preventDefault();
-    renderCard({name: formPlace.value, link: formLink.value});
-    formPlace.value = '';
-    formLink.value = ''
-    openOrCloseForm(popupWindow, formNewPlace);
+    profileName.textContent = formName.value;
+    profileMetier.textContent = formMetier.value;
+    closePopup(event);
+}
+function addNewCard (event) {
+    event.preventDefault();
+    cards.prepend(createCard({name: formPlace.value, link: formLink.value}));
+    formNewPlace.reset();
+    closePopup(event);
 }
 function openImage (image) {
-    popupImage.classList.toggle("popup-image_active");
-    body.classList.toggle("body_no-scroll");
-    popupImage.querySelector(".popup-image__image").src = image.src;
-    popupImage.querySelector(".popup-image__image").alt = image.parentNode.querySelector(".card__title").textContent;
-    popupImage.querySelector(".popup-image__title").textContent = image.parentNode.querySelector(".card__title").textContent;
+  openPopup(popupImage);
+  imageInPopupImage.src = image.src;
+  imageInPopupImage.alt = image.parentNode.querySelector(".card__title").textContent;
+  titleInPopupImage.textContent = image.parentNode.querySelector(".card__title").textContent;
 }
 initialCards.forEach(card => renderCard(card));
-editButton.addEventListener("click", () => {openOrCloseForm(popupWindow, formProfile)});
-addButton.addEventListener("click", () => {openOrCloseForm(popupWindow, formNewPlace)});
-formCloser.addEventListener("click", () => {openOrCloseForm(popupWindow, activeForm)});
-imageCloser.addEventListener("click", ()=> {popupImage.classList.toggle("popup-image_active");  body.classList.toggle("body_no-scroll");});
+popupClosers.forEach(item => item.addEventListener("click", closePopup));
+editButton.addEventListener("click", () => {openPopup(popupProfileEditor)});
+addImageButton.addEventListener("click", () => {openPopup(popupNewCard)});
 formProfile.addEventListener("submit", saveProfile);
 formNewPlace.addEventListener("submit", addNewCard);
